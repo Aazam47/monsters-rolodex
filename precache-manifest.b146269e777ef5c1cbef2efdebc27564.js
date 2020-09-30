91f787b7ebb620f799932e05b70142ae67cfc1ea
Aazam47 <aazam47@hotmail.com>
2020-09-30 11:01:15 +0800

    Updates

---
+++ precache-manifest.b146269e777ef5c1cbef2efdebc27564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e2424fc126055999b506a560f1309b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fa055bd06817192d2bdc",
+    "url": "/monsters-rolodex/static/css/main.01bd677a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa055bd06817192d2bdc",
+    "url": "/monsters-rolodex/static/js/main.2ea50003.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
